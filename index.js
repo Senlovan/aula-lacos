b1690bb6fc33d0b11977d6673ed89b0d6e798a75
Senlovan <maiconmattossenlovan@gmail.com>
2023-05-16 21:16:51 -0300

    Subindo Aula de Laços

---
+++ index.js
@@ -0,0 +1,53 @@
+// let i = 0
+// while (i-10){
+//     console.log (i)
+//     i++
+//     //i = i + 1
+// }
+
+// let numero
+// let somatorio=0
+// while(numero!==0){
+//    numero=Number(prompt("Digite um número:"))
+// // somatorio = somatorio + numero
+// somatorio +=numero
+// }
+
+// console.log(`O resultado da soma é:${somatorio}`);
+
+// const numeros = [10,42,53,68,87];
+// for(let i = 0;i<10;i++){
+//     console.log(numeros);
+// }
+
+// const numeros = [10,42,53,68,87,9,145,24,90];
+// for(let i = 0;i<numeros.length;i++){
+//     console.log(numeros[i]);
+// }
+
+
+// const numero =[11, 15, 18, 14, 12, 13]
+// function maiorNumero(arrayNumeros){
+// let maiorNumero = 0
+// for(let i = 0 ; i <numero.length ; i++){
+//     if(numero[i] >maiorNumero){
+//     maiorNumero = numero[i]
+//     }
+// }
+// console.log(`O maior número do array é: ${maiorNumero}`);
+// }
+// maiorNumero(numero)
+
+
+// const numeros = [14, 67 , 89, 15, 23]
+// for (let numero of numeros){
+//     console.log(numero);
+// }
+
+
+// const frase = ["Oi", "Sumido","Tudo", "bem?", "Saudades!"]
+// let mensagemCompleta = ""
+// for (let mensagem of frase){
+//     mensagemCompleta = mensagemCompleta += mensagem + " "
+// }
+// console.log(mensagemCompleta);
